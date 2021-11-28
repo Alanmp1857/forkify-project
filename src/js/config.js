548271e1d78cfcca5dd83998280626e2f4854719
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2021-11-28 20:48:09 +0530

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5c3ebd39-235a-474f-8520-6c5b59cd74c8';
+export const MODAL_CLOSE_SEC = 2.5;
